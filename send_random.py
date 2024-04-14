df574b526b229e73b298d208dba825b4a3bb84f4
ridwaanhall <erbyl14@gmail.com>
2024-04-14 19:45:47 +0700

    updated with perfect

--- send_random.py
+++ send_random.py
@@ -1,75 +1,35 @@
 import os
+import sys
 import random
 import string
 import time
 import logging
-import argparse
 from dotenv import load_dotenv
 import requests
 from fake_useragent import UserAgent
 
-class RequestSender:
-    def __init__(self, url):
-        self.url = url
-        self.user_agent = UserAgent()
-        self.session = requests.Session()
-        self.session.headers = {'User-Agent': self.user_agent.random}
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s - %(levelname)s] %(message)s')
 
-    def send_request(self, username, question, deviceId, gameSlug='', referrer=''):
-        headers = self._generate_headers(username)
-        data = {
-            'username': username,
-            'question': question,
-            'deviceId': deviceId,
-            'gameSlug': gameSlug,
-            'referrer': referrer
-        }
-        try:
-            response = requests.post(self.url, headers=headers, data=data)
-            response.raise_for_status()
-            return response
-        except requests.exceptions.HTTPError as errh:
-            self.logger.error(f"HTTP Error: {errh}")
-        except requests.exceptions.ConnectionError as errc:
-            self.logger.error(f"Error Connecting: {errc}")
-        except requests.exceptions.Timeout as errt:
-            self.logger.error(f"Timeout Error: {errt}")
-        except requests.exceptions.RequestException as err:
-            self.logger.error(f"OOps: Something Else {err}")
-
-    def send_request_with_retry(self, username, question, deviceId, gameSlug='', referrer='', max_retries=3):
-        retries = 0
-        while retries < max_retries:
-            response = self.send_request(username, question, deviceId, gameSlug, referrer)
-            if response and response.status_code == 429:
-                retry_after = int(response.headers.get('Retry-After', 10))
-                self.logger.info(f"Rate limited. Retrying after {retry_after} seconds")
-                time.sleep(retry_after)
-                retries += 1
-            elif response and response.status_code == 200:
-                return response
-            else:
-                break
-        self.logger.error("Max retries reached. Failed to send request.")
-        return None
-
-    def _generate_headers(self, username):
-        return {
-            'User-Agent': self.user_agent.random,
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Dnt': '1',
-            'Referer': f'https://ngl.link/{username}'
-        }
-
-class DeviceIDGenerator:
-    @staticmethod
-    def generate_deviceId():
-        parts = [8, 4, 4, 4, 12]
-        return '-'.join(''.join(random.choices(string.ascii_lowercase + string.digits, k=part)) for part in parts)
 
 class MessageGenerator:
-    def __init__(self, messages=None):
-        self.messages = messages if messages else [
+    def __init__(self):
+        self.generic_messages = [
+            "Hey there! Just wanted to drop a quick message to say hi and see how you're doing. Hope you're having a great day! 😊",
+            "Hi friend! Sending some positive vibes your way. Hope you're doing well!",
+            "Hey buddy! Remember, you're awesome and capable of achieving anything you set your mind to! 💪",
+            "Hello! Just checking in to see how you're doing. Let me know if there's anything I can do to support you!",
+            "Hi! Wishing you a fantastic day filled with joy and laughter. Keep shining bright! ✨",
+            "Hey! Hope your day is as amazing as you are! 😄",
+            "Hi there! Just wanted to remind you that you're appreciated and valued. Keep being awesome!",
+            "Hey friend! Just dropping by to say hello and spread some positivity your way. Have a wonderful day!",
+            "Hello! Sending you lots of love and good vibes. You've got this!",
+            "Hi! Remember, every challenge you face is an opportunity to grow stronger. Keep pushing forward!",
+            "Hey there! Just wanted to send a quick virtual hug your way. You're not alone, and I'm here for you!",
+            "Hi friend! Hope your day is filled with laughter, love, and all the good things in life. You deserve it!"
+        ]
+        
+        self.messages_hacker = [
             "Initiating secure connection. Your digital presence is attracting attention. Stay vigilant. - ShadowCipher",
             "Warning: Unusual activity detected in your online behavior. Exercise caution. - DarkNetOp",
             "Cipher protocol activated. Your online actions have drawn scrutiny. Maintain discretion. - GhostHacker",
@@ -114,34 +74,178 @@ class MessageGenerator:
         ]
 
     def generate_message(self):
-        return random.choice(self.messages)
+        if random.random() < 1:
+            return random.choice(self.messages_hacker)
+        else:
+            return random.choice(self.generic_messages)
 
-def setup_logging():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+class GameSlugGenerator:
+    def __init__(self):
+        self.game_slugs = [
+            "",  
+            "confessions",  
+            "3words",  
+            "tbh",  
+            "shipme",  
+            "yourcrush",  
+            "cancelled",  
+            "dealbreaker"  
+        ]
+        
+    def generate_game_slug(self):
+        return random.choice(self.game_slugs)
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Send automated messages using specified configurations.")
-    parser.add_argument("-u", "--username", required=True, help="Target username")
-    parser.add_argument("-m", "--message", help="Specific message to send. Randomized if not specified.")
-    parser.add_argument("-c", "--count", type=int, default=1, help="Number of messages to send")
-    parser.add_argument("-s", "--slug", help="Game slug to use. Randomized if not specified.")
-    return parser.parse_args()
+
+class RequestSender:
+    '''
+    This class sends requests to the specified URL.
+    '''
+    def __init__(self, url):
+        '''
+        Initializes the RequestSender with the specified URL.
+        '''
+        if not url:
+            logging.error("URL must be provided")
+            sys.exit(1)
+        self.url = url
+        self.user_agent = UserAgent()
+
+    def send_request(self, username, question, device_id, game_slug, referrer=''):
+        '''
+        Sends a POST request to the specified URL with the provided parameters.
+        '''
+        headers = self._generate_headers(username)
+        data = {
+            'username': username,
+            'question': question,
+            'deviceId': device_id,
+            'gameSlug': game_slug,
+            'referrer': referrer
+        }
+        try:
+            response = requests.post(self.url, headers=headers, data=data)
+            response.raise_for_status()  # This will handle HTTP errors which are 400 and above
+        except requests.exceptions.HTTPError as e:
+            logging.error(f"HTTP Error: {e}")
+            return None
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Request Failed: {e}")
+            return None
+        return response
+
+    def send_request_with_retry(self, username, question, device_id, game_slug, referrer='', max_retries=3):
+        '''
+        Sends a POST request to the specified URL with retry the provided parameters.
+        '''
+        retries = 0
+        while retries < max_retries:
+            response = self.send_request(username, question, device_id, game_slug, referrer)
+            if response is None:
+                logging.info("Request failed, retrying...")
+                retries += 1
+                time.sleep(2)
+                continue
+            
+            if response.status_code == 404:
+                logging.error("HTTP Error 404: Not Found. Stopping the program.")
+                sys.exit(1)
+            
+            if response.status_code == 429:
+                retry_after = int(response.headers.get('Retry-After', 10))
+                logging.info(f"Rate limited. Retrying after {retry_after} seconds...")
+                time.sleep(retry_after)
+                retries += 1
+                continue
+
+            return response
+
+        logging.error("Max retries reached. Failed to send request.")
+        return None
+
+    def _generate_headers(self, username):
+        '''
+        Generates headers for the request.
+        '''
+        return {
+            'Accept': '*/*',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Dnt': '1',
+            'Referer': f'https://ngl.link/{username}',
+            'Sec-Ch-Ua': random.choice(['"Microsoft Edge";v="123"', '"Not:A-Brand";v="8"', '"Chromium";v="123"']),
+            'Sec-Ch-Ua-Mobile': random.choice(['?0', '?1']),
+            'Sec-Ch-Ua-Platform': random.choice(['"Windows"', '"Linux"', '"Macintosh"', '"Android"', '"iOS"']),
+            'User-Agent': self.user_agent.random
+        }
+
+class DeviceIDGenerator:
+    '''
+    Generates device ID for the request.
+    '''
+    @staticmethod
+    def generate_device_id():
+        return '-'.join([''.join(random.choices(string.ascii_lowercase + string.digits, k=part_length))
+                         for part_length in [8, 4, 4, 4, 12]])
 
 if __name__ == "__main__":
     load_dotenv()
-    setup_logging()
-    args = parse_arguments()
-
     url = os.getenv("URL")
+    if not url:
+        logging.error("No URL provided in environment. Exiting.")
+        sys.exit(1)
+    pesan = '''
+    For better experience, please use a valid username.
+    '''
+    print(pesan)
     request_sender = RequestSender(url)
-    message_generator = MessageGenerator()
+    username = input("Enter target username: ").strip()
+    if not username:
+        logging.error("Username is required. Exiting.")
+        sys.exit(1)
+    
+    spam_choice = input("Do you want to spam? (yes/no): ").lower().strip()
+    if spam_choice not in ["yes", "no", "y", "n", ""]:
+        logging.error("Invalid choice for spam. Exiting.")
+        sys.exit(1)
 
-    for _ in range(args.count):
-        message = args.message or message_generator.generate_message()
-        gameSlug = args.slug or ""
-        deviceId = DeviceIDGenerator.generate_deviceId()
-        response = request_sender.send_request_with_retry(args.username, message, deviceId, gameSlug)
+    device_generator = DeviceIDGenerator()
+    message_generator = MessageGenerator()
+    game_slug_generator = GameSlugGenerator()
+    if spam_choice in ["yes", "y", ""]:
+        spam_count = input("How many times do you want to spam? (Default is 9999): ")
+        spam_count = int(spam_count) if spam_count.isdigit() else 9999
+
+        # Determine the number of digits needed for the spam count
+        count_format = f'{{:0{len(str(spam_count))}d}}'
+
+        for i in range(spam_count):
+            device_id = device_generator.generate_device_id()
+            message_input = message_generator.generate_message()
+            game_slug = game_slug_generator.generate_game_slug()
+            
+            response = request_sender.send_request_with_retry(username, message_input, device_id, game_slug)
+            if response:
+                try:
+                    response_data = response.json()
+                    question_id = response_data.get("questionId", "Unknown ID")
+                    user_region = response_data.get("userRegion", "Unknown Region")
+                    logging.info(f"({count_format.format(i+1)} of {count_format.format(spam_count)}) {response.status_code} {response.reason} {user_region} {username.upper()} -> {game_slug.upper()} '{message_input.upper()}'")
+                except ValueError:
+                    logging.error("Failed to decode JSON from response.")
+            else:
+                logging.error("Failed to send message.")
+    else:
+        device_id = device_generator.generate_device_id()
+        message_input = message_generator.generate_message()
+        game_slug = game_slug_generator.generate_game_slug()
+        
+        response = request_sender.send_request_with_retry(username, message_input, device_id, game_slug)
         if response:
-            logging.info(f'Successfully sent: {response.status_code} - {response.text}')
+            try:
+                response_data = response.json()
+                question_id = response_data.get("questionId", "Unknown ID")
+                user_region = response_data.get("userRegion", "Unknown Region")
+                logging.info(f"{response.status_code} {response.reason} {user_region} {username.upper()} {game_slug.upper()} -> '{message_input.upper()}'")
+            except ValueError:
+                logging.error("Failed to decode JSON from response.")
         else:
-            logging.error('Failed to send the message')
+            logging.error("Failed to send message.")
