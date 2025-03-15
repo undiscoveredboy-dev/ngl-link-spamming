d68f8155ede0cca2f2cd59292cade71bba6d2059
undiscoveredboy-dev <102147126+undiscoveredboy-dev@users.noreply.github.com>
2025-03-15 08:10:28 +0530

    Update send_random.py

--- send_random.py
+++ send_random.py
@@ -1,11 +1,3 @@
-'''
-created by: ridwaanhall
-date      : 16/04/2024
-instagram : ridwaanhall
-
-note: don't delete this watermark
-'''
-
 import os
 import sys
 import random
@@ -16,16 +8,15 @@ import requests
 from fake_useragent import UserAgent
 from generator import MessageGenerator, GameSlugGenerator, DeviceIDGenerator, UserRegionGenerator
 
+# Predefined variables
+username = "hypnotic71610"  # Set your target username here
+spam_choice = "yes"  # Set "yes" to enable spam mode, "no" for single message
+spam_count = 10  # Set number of spam messages if spam_choice is "yes"
+
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s - %(levelname)s] %(message)s')
 
 class RequestSender:
-    '''
-    This class sends requests to the specified URL.
-    '''
     def __init__(self, url):
-        '''
-        Initializes the RequestSender with the specified URL.
-        '''
         if not url:
             logging.error("URL must be provided")
             sys.exit(1)
@@ -33,9 +24,6 @@ class RequestSender:
         self.user_agent = UserAgent()
 
     def send_request(self, username, question, device_id, game_slug, referrer=''):
-        '''
-        Sends a POST request to the specified URL with the provided parameters.
-        '''
         headers = self._generate_headers(username)
         data = {
             'username': username,
@@ -47,20 +35,12 @@ class RequestSender:
         try:
             response = requests.post(self.url, headers=headers, data=data)
             response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            logging.error(f"HTTP Error: {e}")
-            # print(response.text)
-            return response
         except requests.exceptions.RequestException as e:
             logging.error(f"Request Failed: {e}")
-            # print(response.text)
-            # return response
+            return None
         return response
 
     def send_request_with_retry(self, username, question, device_id, game_slug, referrer='', max_retries=3):
-        '''
-        Sends a POST request to the specified URL with retry the provided parameters.
-        '''
         retries = 0
         while retries < max_retries:
             response = self.send_request(username, question, device_id, game_slug, referrer)
@@ -69,27 +49,17 @@ class RequestSender:
                 retries += 1
                 time.sleep(2)
                 continue
-            
-            if response.status_code == 404:
-                logging.error("HTTP Error 404: Not Found. Stopping the program.")
-                sys.exit(1)
-            
-            if response.status_code == 429:
-                retry_after = int(response.headers.get('Retry-After', 10))
-                logging.info(f"Rate limited. Retrying after {retry_after} seconds...")
+            if response.status_code in [404, 429]:
+                retry_after = int(response.headers.get('Retry-After', 10)) if response.status_code == 429 else 0
+                logging.info(f"Retrying after {retry_after} seconds...")
                 time.sleep(retry_after)
                 retries += 1
                 continue
-
             return response
-
         logging.error("Max retries reached. Failed to send request.")
         return None
 
     def _generate_headers(self, username):
-        '''
-        Generates headers for the request.
-        '''
         return {
             'Accept': '*/*',
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
@@ -107,64 +77,29 @@ if __name__ == "__main__":
     if not url:
         logging.error("No URL provided in environment. Exiting.")
         sys.exit(1)
-    pesan = '''
-    For better experience, please use a valid username.
-    '''
-    print(pesan)
-    request_sender = RequestSender(url)
-    username = input("Enter target username: ").strip().lower()
-    if not username:
-        logging.error("Username is required. Exiting.")
-        sys.exit(1)
     
-    spam_choice = input("Do you want to spam? (yes/no): ").lower().strip()
-    if spam_choice not in ["yes", "no", "y", "n", ""]:
-        logging.error("Invalid choice for spam. Exiting.")
-        sys.exit(1)
-
+    request_sender = RequestSender(url)
     device_generator = DeviceIDGenerator()
     message_generator = MessageGenerator()
     game_slug_generator = GameSlugGenerator()
-    if spam_choice in ["yes", "y", ""]:
-        spam_count = input("How many times do you want to spam? (Default is 9999): ")
-        print()
-        spam_count = int(spam_count) if spam_count.isdigit() else 9999
-
+    
+    if spam_choice.lower() in ["yes", "y"]:
         count_format = f'{{:0{len(str(spam_count))}d}}'
-
         for i in range(spam_count):
             device_id = device_generator.generate_device_id()
             message_input = message_generator.generate_message()
             game_slug = game_slug_generator.generate_game_slug()
-            
             response = request_sender.send_request_with_retry(username, message_input, device_id, game_slug)
             if response:
-                try:
-                    response_data = response.json()
-                    question_id = response_data.get("questionId", "Unknown ID")
-                    
-                    # Get user region code from the response and use the generator to get the full country name
-                    user_region_code = response_data.get("userRegion", "Unknown Region")
-                    user_region_name = UserRegionGenerator.get_country_name(user_region_code)
-                    
-                    logging.info(f"({count_format.format(i+1)} of {count_format.format(spam_count)}) {response.status_code} {response.reason} {username.upper()} FROM {user_region_name.upper()} -> '{game_slug.upper()}'") # '{message_input.upper()}'
-                except ValueError:
-                    logging.error("Failed to decode JSON from response.")
+                logging.info(f"({count_format.format(i+1)} of {count_format.format(spam_count)}) {response.status_code} {response.reason} {username.upper()} -> '{game_slug.upper()}'")
             else:
                 logging.error("Failed to send message.")
     else:
         device_id = device_generator.generate_device_id()
         message_input = message_generator.generate_message()
         game_slug = game_slug_generator.generate_game_slug()
-        
         response = request_sender.send_request_with_retry(username, message_input, device_id, game_slug)
         if response:
-            try:
-                response_data = response.json()
-                question_id = response_data.get("questionId", "Unknown ID")
-                user_region = response_data.get("userRegion", "Unknown Region")
-                logging.info(f"{response.status_code} {response.reason} {user_region} {username.upper()} {game_slug.upper()} -> '{message_input.upper()}'")
-            except ValueError:
-                logging.error("Failed to decode JSON from response.")
+            logging.info(f"{response.status_code} {response.reason} {username.upper()} {game_slug.upper()} -> '{message_input.upper()}'")
         else:
             logging.error("Failed to send message.")
